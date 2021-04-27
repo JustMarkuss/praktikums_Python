0fd4999cfdd4c51ff74913a0888fd4b310d02eb3
JustMarkuss <78404882+JustMarkuss@users.noreply.github.com>
2021-04-27 09:43:01 +0300

    Update uzd5.py

--- uzd5.py
+++ uzd5.py
@@ -3,3 +3,8 @@ Uzrakstiet programmu Python,
 kas pieņem veselu skaitli (n) un 
 aprēķina n + nn + nnn vērtību.
 """
+sk1=float(input("Ievadi skaitli:"))
+
+summa=sk1+(sk1*sk1)+(sk1*sk1*sk1)
+
+print(summa)
